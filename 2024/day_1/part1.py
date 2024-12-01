f34cbf4e18554afabb19de457987e79ffc6dae47
Dawit-y <dawityimer52@gmail.com>
2024-12-02 00:17:40 +0300

    refactor

--- 2024/day_1/part1.py
+++ 2024/day_1/part1.py
@@ -1,32 +1,19 @@
+with open("2024/day_1/input.txt", "r") as f:
+    lines = [line.strip().split("   ") for line in f]
 
-f = open("2024/day_1/input.txt", "r")
-lines = f.readlines()
-left, right = [], []
-for line in lines:
-    modified_lines = line.strip().split("   ")
-    left.append(int(modified_lines[0]))
-    right.append(int(modified_lines[1]) )
+left = sorted(int(line[0]) for line in lines)
+right = sorted(int(line[1]) for line in lines)
 
-left.sort()
-right.sort()
-difference_list = []
-for i in range(len(left)):
-    diff =  abs(left[i] - right[i]) 
-    difference_list.append(diff)
-
-sum_of_difference = sum(difference_list)
+# Part 1
+sum_of_difference = sum(abs(l - r) for l, r in zip(left, right))
 print(sum_of_difference)
 
-# part 2
+# Part 2
+from collections import Counter
 
-scores = []
-count = 0
-for i in left:
-    for j in right:
-        if i == j:
-            count+=1
-    scores.append(count * i)
-    count = 0   
+left_count = Counter(left)
+right_count = Counter(right)
 
-total_similarity_score = sum(scores)
+total_similarity_score = sum( value * right_count[value] for value in left_count)
 print(total_similarity_score)
+
